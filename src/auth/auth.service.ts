69d74be7e4ad6dce7e2294f1ff4763d414c9ca07
levorah <levora004@gmail.com>
2025-03-06 10:11:02 +0000

    feat: interface created for our newly creating model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { User } from "./interface";
 
 
 @Injectable({})
 
 export class AuthService {
     constructor(
-        @InjectModel('User') private readonly userModel: Model<any>
+        @InjectModel('User') private readonly userModel: Model<User>
     ) { }
     login() {
-        return
+
+        return 
     }
 }
--- src/auth/schema/auth.schema.ts
+++ src/auth/schema/auth.schema.ts
@@ -4,5 +4,6 @@ export const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    gender: { type: String, required: true }
+    gender: { type: String, required: true },
+
 })
