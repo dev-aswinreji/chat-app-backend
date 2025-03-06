6bf1528417180cfc31a52131ec4b10da62308c74
levorah <levora004@gmail.com>
2025-03-06 13:06:08 +0000

    feat: creating jwt token verfication for our app

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,19 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserSchema } from "./schema";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./strategy";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: "User", schema: UserSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: "User", schema: UserSchema }]),
+        JwtModule.register({})
+    ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [
+        AuthService,
+        JwtStrategy
+    ]
 })
 
 export class AuthModule { }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AuthService } from "../auth.service";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "../interface";
+
+@Injectable({})
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(
+        config: ConfigService,
+        @InjectModel('User') private readonly userModel: Model<User>
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_SECRET')!
+        })
+    }
+    async validate(payload: { sub: number, username: string }) {
+        const user = await this.userModel.findById(payload.sub)
+        console.log(user, 'user is here')
+    }
+}
