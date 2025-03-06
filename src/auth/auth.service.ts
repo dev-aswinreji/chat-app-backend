8cf2be927b406cba04ca0ab9151eb0b15072617f
levorah <levora004@gmail.com>
2025-03-06 09:59:35 +0000

    feat: schema updated need to make the model injection and query data from db need to check that

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 
 @Injectable({})
 
 export class AuthService {
-    login(){
-        return 'login success from servie layer'
+    constructor(
+        @InjectModel('User') private readonly userModel: Model<any>
+    ) { }
+    login() {
+        return
     }
 }
