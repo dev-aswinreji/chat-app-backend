237094c83496248f2bd28e75524213ee1f2a211f
levorah <levora004@gmail.com>
2025-03-09 13:38:13 +0000

    feat: unfortunately we fixed some of issue regarding message schema in converstation schema branch

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MessageSchema } from "./schema";
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: "Message", schema: MessageSchema }])],
 })
 
 export class MessageModule { }
